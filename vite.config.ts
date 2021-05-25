479e76a6ae2867e5afb5fbda96cb872f9adb666d
Nick Graffis <nicholasgraffis@gmail.com>
2021-05-24 21:31:23 -0700

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,164 @@
+import path from 'path'
+import fs from 'fs'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
+import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
+import ViteComponents from 'vite-plugin-components'
+import Markdown from 'vite-plugin-md'
+import WindiCSS from 'vite-plugin-windicss'
+import { VitePWA } from 'vite-plugin-pwa'
+import VueI18n from '@intlify/vite-plugin-vue-i18n'
+import Prism from 'markdown-it-prism'
+import matter from 'gray-matter'
+import anchor from 'markdown-it-anchor'
+import markdownAttr from 'markdown-it-link-attributes'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  server: {
+    open: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8888/.netlify/functions',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  plugins: [
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages({
+      extensions: ['vue', 'md'],
+      extendRoute(route) {
+        const p = path.resolve(__dirname, route.component.slice(1))
+
+        if (!p.includes('projects.md')) {
+          const md = fs.readFileSync(p, 'utf-8')
+          const { data } = matter(md)
+          const string = md.split('')
+          const length = string.length
+          const words = length / 6.7
+          const min = words / 200
+          const frontmatter = data
+          frontmatter.duration = `${Math.ceil(min)} min read`
+          route.meta = Object.assign(route.meta || {}, { frontmatter })
+        }
+
+        return route
+      },
+    }),
+
+    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+    Layouts(),
+
+    // https://github.com/antfu/vite-plugin-md
+    Markdown({
+      wrapperComponent: 'post',
+      wrapperClasses: 'prose w-full',
+      headEnabled: true,
+      markdownItOptions: {
+        quotes: '""\'\'',
+      },
+      markdownItSetup(md) {
+        md.use(Prism)
+        md.use(anchor, {
+          permalink: true,
+          permalinkBefore: true,
+          permalinkSymbol: '#',
+        })
+        md.use(markdownAttr, {
+          pattern: /^https?:/,
+          attrs: {
+            target: '_blank',
+            rel: 'noopener',
+          },
+        })
+      },
+    }),
+
+    // https://github.com/antfu/vite-plugin-components
+    ViteComponents({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+
+      // allow auto import and register components used in markdown
+      customLoaderMatcher: id => id.endsWith('.md'),
+
+      // auto import icons
+      customComponentResolvers: [
+        // https://github.com/antfu/vite-plugin-icons
+        ViteIconsResolver({
+          componentPrefix: '',
+          // enabledCollections: ['carbon']
+        }),
+      ],
+    }),
+
+    // https://github.com/antfu/vite-plugin-icons
+    ViteIcons(),
+
+    // https://github.com/antfu/vite-plugin-windicss
+    WindiCSS({
+      safelist: 'prose prose-sm m-auto text-left',
+    }),
+
+    // https://github.com/antfu/vite-plugin-pwa
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Nick Graffis',
+        short_name: 'nickgraffis',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+
+    // https://github.com/intlify/vite-plugin-vue-i18n
+    VueI18n({
+      include: [path.resolve(__dirname, 'locales/**')],
+    }),
+  ],
+  // https://github.com/antfu/vite-ssg
+  ssgOptions: {
+    script: 'async',
+    formatting: 'minify',
+  },
+
+  optimizeDeps: {
+    include: [
+      'vue',
+      'vue-router',
+      '@vueuse/core',
+    ],
+    exclude: [
+      'vue-demi',
+    ],
+  },
+})
