f5c9bdeaa18312d3127923685ce000444e1db36b
Nick Graffis <nicholasgraffis@gmail.com>
2021-09-21 20:15:56 -0700

    changes

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -6,7 +6,7 @@ const router = useRouter()
 <template>
   <div class="w-screen flex-col space-y-12 bg-yellow-400 h-screen lg:text-10xl md:text-8xl text-5xl flex items-center justify-center text-green-800">
     <a class="top-36 z-40 text-2xl px-6 text-center font-semibold" @click="router.back()">
-      The page you are looking for cannot be found.
+      🤷‍♂️
     </a>
     <p class="font-black z-40 px-6 text-center">
       404
