c35c8c99ca601a0530d34b06e0180088b5f5642c
Nick Graffis <hi@nickgraffis.me>
2022-07-13 16:53:11 -0700

    calendar work

---
+++ src/pages/cal.vue
@@ -0,0 +1,13 @@
+---
+title: Calendar
+layout: cal
+---
+
+<route lang="yaml">
+meta:
+  layout: cal
+</route>
+
+<template>
+  <Calendar />
+</template>
---
+++ src/pages/news/girls-cif-finals.md
@@ -0,0 +1,7 @@
+---
+title: Girls come up short in CIF Finals of Division II
+date: 03-01-2022
+lang: en
+---
+
+## The Girls
